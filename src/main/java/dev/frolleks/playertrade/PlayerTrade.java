19ef9fb94dc8b66b693c313e8686f9ae3ea7ebad
frolleks <frolleks@hotmail.com>
2024-04-16 09:48:02 +0700

    init

---
+++ src/main/java/dev/frolleks/playertrade/PlayerTrade.java
@@ -0,0 +1,8 @@
+package dev.frolleks.playertrade;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PlayerTrade implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
---
+++ src/main/java/dev/frolleks/playertrade/client/PlayerTradeClient.java
@@ -0,0 +1,8 @@
+package dev.frolleks.playertrade.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerTradeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
